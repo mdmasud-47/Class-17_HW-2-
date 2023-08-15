46c49c0c4ceab08e1a0b6f5d599160ad8465cc9c
mdmasud-47 <mdmoksod77@gmail.com>
2023-08-15 21:41:21 +0600

    ..

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.getElementById('button').addEventListener('click', function(){
+  document.getElementById('sidebar').style.marginLeft = 0;
+}) 
+
+document.getElementById('button').addEventListener('dblclick', function(){
+  document.getElementById('sidebar').style.marginLeft = '-300px' ;
+})
